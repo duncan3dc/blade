a80065b5e1e0e4094d686f110477ed5784ed5cc4
Craig Duncan <git@duncanc.co.uk>
2014-09-28 19:40:10 +0100

    Added some basic tests for the Blade class

---
+++ tests/BladeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace duncan3dc\Laravel;
+
+use duncan3dc\Helpers\Env;
+
+class BladeTest extends \PHPUnit_Framework_TestCase
+{
+    public function __construct()
+    {
+        Env::usePath(__DIR__);
+    }
+
+
+    public function testView1()
+    {
+        $result = Blade::make("view1")->__toString();
+        $this->assertSame(file_get_contents(__DIR__ . "/views/view1.blade.php"), $result);
+    }
+
+
+    public function testView2()
+    {
+        $result = Blade::make("view2", ["title" => "Test Title"])->__toString();
+        $this->assertSame(file_get_contents(__DIR__ . "/views/view2.html"), $result);
+    }
+}
