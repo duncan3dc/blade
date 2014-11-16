daa1ae9ff89fe976688d429913a8ba725f4383b9
Craig Duncan <git@duncanc.co.uk>
2014-11-16 00:26:42 +0000

    Updated the tests to use the render() method

--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -14,14 +14,14 @@ class BladeTest extends \PHPUnit_Framework_TestCase
 
     public function testBasic()
     {
-        $result = Blade::make("view1")->__toString();
+        $result = Blade::make("view1")->render();
         $this->assertSame(file_get_contents(__DIR__ . "/views/view1.blade.php"), $result);
     }
 
 
     public function testParameters()
     {
-        $result = Blade::make("view2", ["title" => "Test Title"])->__toString();
+        $result = Blade::make("view2", ["title" => "Test Title"])->render();
         $this->assertSame(file_get_contents(__DIR__ . "/views/view2.html"), $result);
     }
 
@@ -29,21 +29,21 @@ class BladeTest extends \PHPUnit_Framework_TestCase
     public function testAltPath()
     {
         Blade::addPath(__DIR__ . "/views/alt");
-        $result = Blade::make("view3")->__toString();
+        $result = Blade::make("view3")->render();
         $this->assertSame(file_get_contents(__DIR__ . "/views/alt/view3.blade.php"), $result);
     }
 
 
     public function testNamespace()
     {
-        $result = Blade::make("view4")->__toString();
+        $result = Blade::make("view4")->render();
         $this->assertSame("duncan3dc\\Laravel", $result);
     }
 
 
     public function testUse()
     {
-        $result = Blade::make("view5")->__toString();
+        $result = Blade::make("view5")->render();
         $this->assertSame(Env::getMachineName(), $result);
     }
 
