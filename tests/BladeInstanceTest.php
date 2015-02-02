98e9e87d8c4d6b7c267c451024674763e4cd9899
Craig Duncan <git@duncanc.co.uk>
2015-02-02 13:01:58 +0000

    Add tests for the new BladeInstance class

--- tests/BladeInstanceTest.php
+++ tests/BladeInstanceTest.php
@@ -4,86 +4,88 @@ namespace duncan3dc\Laravel;
 
 use duncan3dc\Helpers\Env;
 
-class BladeTest extends \PHPUnit_Framework_TestCase
+class BladeInstanceTest extends \PHPUnit_Framework_TestCase
 {
+    protected $blade;
 
     public function setUp()
     {
-        Env::usePath(__DIR__);
+        Env::usePath("/tmp");
+        $this->blade = new BladeInstance(__DIR__ . "/views");
     }
 
     public function testBasicMake()
     {
-        $result = Blade::make("view1")->render();
+        $result = $this->blade->make("view1")->render();
         $this->assertSame(file_get_contents(__DIR__ . "/views/view1.blade.php"), $result);
     }
 
 
     public function testBasicRender()
     {
-        $result = Blade::render("view1");
+        $result = $this->blade->render("view1");
         $this->assertSame(file_get_contents(__DIR__ . "/views/view1.blade.php"), $result);
     }
 
 
     public function testParametersMake()
     {
-        $result = Blade::make("view2", ["title" => "Test Title"])->render();
+        $result = $this->blade->make("view2", ["title" => "Test Title"])->render();
         $this->assertSame(file_get_contents(__DIR__ . "/views/view2.html"), $result);
     }
 
 
     public function testParametersRender()
     {
-        $result = Blade::render("view2", ["title" => "Test Title"]);
+        $result = $this->blade->render("view2", ["title" => "Test Title"]);
         $this->assertSame(file_get_contents(__DIR__ . "/views/view2.html"), $result);
     }
 
 
     public function testAltPath()
     {
-        Blade::addPath(__DIR__ . "/views/alt");
-        $result = Blade::render("view3");
+        $this->blade->addPath(__DIR__ . "/views/alt");
+        $result = $this->blade->render("view3");
         $this->assertSame(file_get_contents(__DIR__ . "/views/alt/view3.blade.php"), $result);
     }
 
 
     public function testNamespace()
     {
-        $result = Blade::render("view4");
+        $result = $this->blade->render("view4");
         $this->assertSame("duncan3dc\\Laravel", trim($result));
     }
 
 
     public function testUse()
     {
-        $result = Blade::render("view5");
+        $result = $this->blade->render("view5");
         $this->assertSame(Env::getMachineName(), trim($result));
     }
 
 
     public function testRawOutput()
     {
-        $result = Blade::render("view6");
+        $result = $this->blade->render("view6");
         $this->assertSame(file_get_contents(__DIR__ . "/views/view6.html"), $result);
     }
 
 
     public function testEscapedOutput()
     {
-        $result = Blade::render("view7");
+        $result = $this->blade->render("view7");
         $this->assertSame(file_get_contents(__DIR__ . "/views/view7.html"), $result);
     }
 
 
     public function testExists1()
     {
-        $this->assertTrue(Blade::exists("view1"));
+        $this->assertTrue($this->blade->exists("view1"));
     }
 
 
     public function testDoesntExist()
     {
-        $this->assertFalse(Blade::exists("no-such-view"));
+        $this->assertFalse($this->blade->exists("no-such-view"));
     }
 }
